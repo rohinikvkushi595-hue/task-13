3760b97510d559cb200531c7903992794f21cf8a
rohinikvkushi595-hue <rohinikvkushi595@gmail.com>
2026-09-21 16:53:22 +0530

    Add files via upload

---
+++ useFetchData.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetchData = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError("");
+
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError("Failed to fetch user data.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetchData;
